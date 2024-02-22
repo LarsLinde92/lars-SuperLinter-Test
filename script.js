f85b31ad05c3236eee9be57f3dcdad30d20a5331
Lars Linde <147490345+LarsLinde92@users.noreply.github.com>
2024-02-22 10:38:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const x = 5
-const y = 7
+const x = 5;
+const y = 7;
 
-function double (x) { return x * 2 }
-                    
-console.print('Dubbelt av 5 är ' + double(x) + double (y)));
+function double(x) {
+  return x * 2;
+}
+
+console.log('Dubbelt av 5 är ' + double(x) + ' och dubbelt av 7 är ' + double(y));
